43c95d8f9f3a3bf235228469be2d8e7500fc4d4e
jasbel <asbel.elmec@gmail.com>
2022-04-28 01:01:30 -0400

    feat: for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Himno Web",
+        short_name: "Himnos",
+        description: "Esta app cuenta con canticos y alabanzas",
+        theme_color: "#efefef",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        sourcemap: true,
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
+});
