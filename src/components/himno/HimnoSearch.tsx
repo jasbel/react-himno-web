0a00a9d9da2bd29f856a6a2f86f3945553762561
jasbel <asbel.elmec@gmail.com>
2022-05-18 01:08:16 -0400

    refact: himno search

--- src/components/himno/HimnoSearch.tsx
+++ src/components/himno/HimnoSearch.tsx
@@ -1,5 +1,5 @@
+import { Flex, Input } from '@chakra-ui/react';
 import React, {useEffect, useState} from 'react';
-// import LinearGradient from 'react-native-linear-gradient';
 import { opacityColor } from '../../helpers/helper';
 import Colors from '../../res/colors';
 import { responsive } from '../../utils/responsive';
@@ -23,42 +23,19 @@ const HimnoSearch = ({onChange, modeSearch}: Props) => {
   }, [modeSearch]);
 
   return (
-    <div style={styles.container}>
-      <input
-        style={{...styles.textInput}}
+    <Flex bg={opacityColor(Colors.bkgWhite, 0.5)} pl={8}>
+      <Input
         value={query}
         placeholder={'Buscar...'}
         onChange={handleText}
         color={Colors.txtDark}
+        my={2}
+        bg={Colors.grayLight}
+        borderRadius={50}
+        fontSize={responsive(20, 16)}
       />
-    </div>
+    </Flex>
   );
-};
-
-const styles : { [key in any]: React.CSSProperties } ={
-  container: {
-    display: 'flex',
-    width: '100%',
-    position: 'relative',
-    borderBottomColor: opacityColor(Colors.bkgWhite, 0.5),
-    borderBottomWidth: 4,
-    marginLeft: 30,
-  },
-  textInput: {
-    display: 'flex',
-    flex: 1,
-    backgroundColor: Colors.grayLight,
-    padding: responsive(12, 8),
-    paddingLeft: 16,
-    paddingRight: 16,
-    color: Colors.txtBlack,
-    fontSize: responsive(20, 18),
-    borderWidth: 0,
-    borderBottomColor: Colors.grayLight,
-    marginTop: 10,
-    marginBottom: 10,
-    borderRadius: 50,
-  },
 };
 
 export default HimnoSearch;
